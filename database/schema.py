a61260bb5341ffeb8e52ca16c349ffcbd1324e24
Lê Bảo Long <140832783+longle325@users.noreply.github.com>
2025-02-01 22:18:48 +0700

    Replace old code

--- database/schema.py
+++ database/schema.py
@@ -3,7 +3,15 @@ from typing import Optional, List
 from uuid import UUID, uuid4
 
 class Company(SQLModel, table=True):
-    name: str = Field(index=True, unique=True,default="",description="Name of the company",primary_key=True)
+    id: UUID = Field(
+        default_factory=uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+        description="Id of the company"
+    )
+
+    name: str = Field(index=True, unique=True,default="",description="Name of the company",primary_key=False)
 
 class User(SQLModel,table=True):
     id: UUID = Field(
@@ -11,6 +19,7 @@ class User(SQLModel,table=True):
         primary_key=True,
         index=True,
         nullable=False,
+        description="Id of the user"
     )
     name: str = Field(default="",description="Name of the user")
     company_name: str = Field(default=None,foreign_key="company.name",description="Name of the company")
